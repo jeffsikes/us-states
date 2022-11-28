db91120e2f6cdd152695740ff66fe51a0e54d208
Jeff Sikes <jeffsikes@users.noreply.github.com>
2022-11-28 00:04:50 -0600

    Updates to node.js file system module. References issue #3.

--- converters/csv-to-sql.js
+++ converters/csv-to-sql.js
@@ -8,10 +8,10 @@ if (!Object.values) {
 
 fs.truncate('data/states.sql', 0, function() {
 
-  fs.appendFile('data/states.sql', 'TRUNCATE TABLE `states`;\n');
+  fs.appendFileSync('data/states.sql', 'TRUNCATE TABLE `states`;\n');
 
   for (var i = 0; i < data.length; i++) {
     var query = 'INSERT INTO `states` (`' + Object.keys(data[i]).join('`, `') + '`) VALUES ("' + Object.values(data[i]).join('", "') + '");\n';
-    fs.appendFile('data/states.sql', query.replace(/""/g, 'null'));
+    fs.appendFileSync('data/states.sql', query.replace(/""/g, 'null'));
   }
 });
--- converters/json-to-xml.js
+++ converters/json-to-xml.js
@@ -4,4 +4,4 @@ var js2xmlparser = require('js2xmlparser');
 
 var xml = js2xmlparser.parse('states', { 'state': obj });
 
-fs.writeFile('data/states.xml', xml);
+fs.writeFileSync('data/states.xml', xml);
